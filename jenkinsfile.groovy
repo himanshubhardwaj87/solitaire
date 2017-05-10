38f37dabc40f99bbe403d6fe83ec4dd584a276ea
himanshubhardwaj87 <himanshubhardwaj87@gmail.com>
2017-05-10 17:56:32 +0530

    added information

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -20,13 +20,33 @@ parallel chrome:{
 }, firfox:{
     runTests("Firefox")
 }
+node {
+    notify("Deploy to staging?")
+}
+
+input 'Deploy to staging?'
+
+// limit concurrency so we don't perform simultaneous deploys
+// and if multiple pipelines are executing, 
+// newest is only that will be allowed through, rest will be canceled
+stage name: 'Deploy to staging', concurrency: 1
+node {
+    // write build number to index page so we can see this update
+bat "echo '<h1>${env.BUILD_DISPLAY_NAME}</h1>' >> app/index.html"
+
+    
+    // deploy to a docker container mapped to port 3000
+ bat 'docker-compose up -d --build'
+   // sh 'docker-compose up -d --build'
+    
+    notify 'Solitaire Deployed!'
+}
+
 notify('Execution Complete')
  
 def runTests(browser) {
     node {
 		try{
-//     bat 'del /S /Q *'
-//     unstash 'everything'
      bat "npm run test-single-run -- --browsers ${browser}"
 
         step([$class: 'JUnitResultArchiver', 
