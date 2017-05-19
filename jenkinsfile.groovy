98ee562315a9173779f8f8761478874f9c881f46
himanshubhardwaj87 <himanshubhardwaj87@gmail.com>
2017-05-19 09:14:01 +0530

    Updated

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,46 +1,47 @@
 stage 'CI'
 node {
-	checkout scm
-	// pull dependencies from npm
-	bat 'npm install'
-}
 
-stage 'PhantomJS Testing'
-node {
-	try{
-	// test with PhantomJS for "fast" "generic" results
-	bat 'npm run test-singl-run -- --browsers PhantomJS'
-	echo 'Succeeded!'
-	 currentBuild.result = 'SUCCESS'
-	}catch (Exception err) {
-         echo "Failed: ${err}"
-		  currentBuild.result = 'FAILURE'
-    }
-	// archive karma test results (karma is configured to export junit xml files)
-	//finally {
-	//step([$class: 'JUnitResultArchiver',
-	//	testResults: 'test-results/**/test-results.xml'])
-	//}
-	stage 'Browser Testing'
-	echo 'Printed whether above succeeded or failed.'
-	runTests("Chrome")
-}
-
- // if (currentBuild.result == null || currentBuild.result == 'SUCCESS' || currentBuild.result == 'FAILURE') {
-//parallel integration testing
-//stage 'Browser Testing'
-
-//runTests("Chrome")
-//node{ echo "{status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}] ${env.BRANCH_NAME} ${env.NODE_NAME}'" }
-//}
-def runTests(browser) {
-
-
-		// on windows use:
-		bat "npm run test-single-run -- --browsers ${browser}"
-
-		step([$class: 'JUnitResultArchiver',
-			testResults: 'test-results/**/test-results.xml'])
+    checkout scm
 	
+    // pull dependencies from npm
+    // on windows use: 
+bat 'npm install'
+    //sh 'npm install'
+
+    
+    
+    // test with PhantomJS for "fast" "generic" results
+    // on windows use: 
+bat 'npm run test-single-run -- --browsers PhantomJS'
+    //sh 'npm run test-single-run -- --browsers PhantomJS'
+    
+    // archive karma test results (karma is configured to export junit xml files)
+    step([$class: 'JUnitResultArchiver', 
+          testResults: 'test-results/**/test-results.xml'])
+          
+}
+
+
+//parallel integration testing
+stage 'Browser Testing'
+
+    runTests("Chrome")
+	node{
+echo "{status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}] ${env.BRANCH_NAME} ${env.NODE_NAME}'"
+}
+def runTests(browser) {
+    node {
+        // on windows use: bat 'del /S /Q *'
+       // sh 'rm -rf *'
+
+        //unstash 'everything'
+
+        // on windows use: 
+		bat "npm run test-single-run -- --browsers ${browser}"
+        //sh "npm run test-single-run -- --browsers ${browser}"
+
+        step([$class: 'JUnitResultArchiver', 
+              testResults: 'test-results/**/test-results.xml'])
+    }
 }
 
