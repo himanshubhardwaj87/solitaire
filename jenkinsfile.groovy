9968066fc94a9caba7e805f4bae49a96a77ddd72
himanshubhardwaj87 <himanshubhardwaj87@gmail.com>
2017-05-19 09:44:56 +0530

    Updated

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,38 +2,34 @@ stage 'CI'
 node {
     checkout scm
     // pull dependencies from npm
-    // on windows use: 
 bat 'npm install'
-    //sh 'npm install'
-    // test with PhantomJS for "fast" "generic" results
-    // on windows use: 
-	stage 'PhantomJS Testing'
-bat 'npm run test-single-run -- --browsers PhantomJS'
-    //sh 'npm run test-single-run -- --browsers PhantomJS'
-    // archive karma test results (karma is configured to export junit xml files)
-    step([$class: 'JUnitResultArchiver', 
-          testResults: 'test-results/**/test-results.xml'])
-          
+    // test with PhantomJS for "fast" "generic" results         
 }
-
-//parallel integration testing
-stage 'Browser Testing'
-  parallel ChromeTest: {runTests("Chrome")},
-		   FirefoxTest: {runTests("Firefox")}
 
 def runTests(browser) {
     node {
-        // on windows use: bat 'del /S /Q *'
-       // sh 'rm -rf *'
-
-        //unstash 'everything'
-
-        // on windows use: 
 		bat "npm run test-single-run -- --browsers ${browser}"
-        //sh "npm run test-single-run -- --browsers ${browser}"
-
         step([$class: 'JUnitResultArchiver', 
               testResults: 'test-results/**/test-results.xml'])
     }
 }
 
+def paralleltask = [:]  
+paralleltask["PhantomJS Testing"] = {  
+    node() {
+       	stage 'PhantomJS Testing'
+bat 'npm run test-single-run -- --browsers PhantomJS'
+    // archive karma test results (karma is configured to export junit xml files)
+    step([$class: 'JUnitResultArchiver', 
+          testResults: 'test-results/**/test-results.xml'])
+    }
+}
+
+paralleltask["Browser Testing"] = {  
+    node() {
+        stage 'Browser Testing'
+		runTests("Chrome")
+    }
+}
+
+parallel paralleltask
